8614b249638f65791567f92412d85443c88929eb
ClaudiaDanciu <contact@claudiadanciu.com>
2026-01-30 19:35:51 +0000

    Update capacitator package for ios

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -5,10 +5,11 @@ const config: CapacitorConfig = {
   appId: 'app.lovable.cdd216a026f645838c70f80577ecc33d',
   appName: 'health-flow-sympa-ai',
   webDir: 'dist',
-  server: {
-    url: 'https://cdd216a0-26f6-4583-8c70-f80577ecc33d.lovableproject.com?forceHideBadge=true',
-    cleartext: true
-  },
+  // Remove server URL for production
+  // server: {
+  //   url: 'https://cdd216a0-26f6-4583-8c70-f80577ecc33d.lovableproject.com?forceHideBadge=true',
+  //   cleartext: true
+  // },
   plugins: {
     Camera: {
       permissions: ['camera', 'photos']
